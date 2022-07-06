683493b55a994b25e22db7f5a4dbf03b803dcb65
Matthew Phelps <matthew.w.phelps18@gmail.com>
2022-07-06 14:12:48 -0500

    Completed random image generator

--- js/script.js
+++ js/script.js
@@ -3,12 +3,38 @@ const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
+// async funtion to get image array (called on button event listener)
+
 const getImage = async function() {
     const res = await fetch(
         "https://picsum.photos/v2/list?limit=100"
     );
     const images = await res.json();
-    console.log(images);
+    selectRandomImage(images);
+    //console.log(images);
 };
 
-getImage();
+// function to select random image (called in getImage function)
+
+const selectRandomImage = function(images) {
+    const randomIndex = Math.floor(Math.random() * images.length)
+    const randomImage = images[randomIndex];
+    //console.log(randomImage);
+    displayImage(randomImage);
+};
+
+// function to display image (called in selectRandomImage funciton)
+
+const displayImage = function(randomImage) {
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+};
+
+// button event listener
+
+button.addEventListener("click", function() {
+    getImage();
+});
